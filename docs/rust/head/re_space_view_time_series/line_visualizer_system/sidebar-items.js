488f8097da921e40bc5c8d3b95222cc24f1649fe
emilk <emilk@users.noreply.github.com>
2024-02-13 12:31:47 +0000

    Update the rust docs

--- docs/rust/head/re_space_view_time_series/line_visualizer_system/sidebar-items.js
+++ docs/rust/head/re_space_view_time_series/line_visualizer_system/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":["DEFAULT_STROKE_WIDTH"],"struct":["SeriesLineSystem"]};
+window.SIDEBAR_ITEMS = {"constant":["DEFAULT_STROKE_WIDTH"],"fn":["load_series"],"struct":["SeriesLineSystem"]};
